ce322ed42515727be95cfc4936792add746b1a1e
Subha Ghanta <subhaghanta60@gmail.com>
2023-02-10 23:34:35 +0530

    first message

---
+++ Assignments/Assignment3/Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+public static void main(String [] args)
+{
+for(int i=89;i>=79;i--)
+{
+System.out.println(i);
+}
+}
+}
---
+++ ForLoop/Name.java
@@ -0,0 +1,10 @@
+class Name
+{
+public static void main(String [] args)
+{
+for(int i=1;i<=3;i++)
+{
+System.out.println("Hello Subha");
+}
+}
+}
